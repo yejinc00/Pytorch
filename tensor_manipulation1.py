8fc57db2906e5dea2332bd598a4696e18bea0fac
new-w <newww0120@gmail.com>
2020-12-30 02:14:08 +0900

    day2

--- tensor_manipulation1.py
+++ tensor_manipulation1.py
@@ -10,3 +10,18 @@ print(m1.matmul(m2))                    # matrix multiplication
 
 print(m1*m2)                            # element-wise multiplication
 print(m1.mul(m2))
+
+t1=torch.FloatTensor([1, 2])
+t2=torch.FloatTensor([[1, 2], [3, 4]])
+print(t1.mean())                        # get average value of elements
+print(t2.mean())                        # 10/4 = 2.5
+
+print(t2.mean(dim=0))                   # remove first dimension(row) by average value
+print(t2.mean(dim=1))                   # remove second dimension(column) by average value
+
+print(t1.sum())
+print(t2.sum())
+print(t2.sum(dim=0))                    #remove first dimension by addition
+print(t2.sum(dim=1))                    #remove second dimension by addition
+
+
